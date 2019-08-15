27118902d44e752bf27a7f8e146bac1cf6cebd6d
slviajero <stefan.lenz@serverfabrik.de>
2019-08-15 18:26:16 +0200

    Editing locally

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ def hello_world(request):
         The response text or any set of values that can be turned into a
         Response object using
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+
+    Trying to do a bit more. 
+
     """
     request_json = request.get_json()
     if request.args and 'message' in request.args:
-        return request.args.get('message')
+	value=request.args.get('message')
+	n=int(value)
+        return n 
     elif request_json and 'message' in request_json:
         return request_json['message']
     else:
